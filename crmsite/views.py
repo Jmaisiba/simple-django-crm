fc01a75df496a4f85e832955b01774f453417e9d
Jack Maisiba <jmaisiba@gmail.com>
2023-10-05 19:28:24 +0300

    Initial setup for registration form

--- crmsite/views.py
+++ crmsite/views.py
@@ -31,3 +31,9 @@ def logout_user(request):
 	logout(request)
 	messages.success(request, "You have Successfully Logged out!!!")
 	return redirect('home')
+
+
+
+def register_user(request):
+
+	return render(request, 'register.html' , {})
